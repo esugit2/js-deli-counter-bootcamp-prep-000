ea3b3144cb27eaebd48342663b9f6e6b78bf2ee2
Edward Sugitpibul <etsugitpibul@gmail.com>
2019-08-16 17:05:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function currentLine(line) {
   if (line.length === 0) {
     return `The line is currently empty.`;
   } else {
-    var i = [];
+    var array = [];
     for (var i = 0; i < line.length; i++) {
       array.push(` ${i+1}. ${line[i]}`);
     }
-    return `The line is currently:` + i;
+    return `The line is currently:` + array;
   }
 }
 
